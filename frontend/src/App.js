e6c47664131dfbacfa500d8c13b66795d972663a
Felix Haeberle <kontakt@felixhaeberle.de>
2022-05-29 17:42:30 +0200

    update localhost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,7 @@ import { getSimilarColors, stringToColor } from "./helpers/colors";
 import io from "socket.io-client";
 
 let socket;
-console.log(process.env.NODE_ENV);
-const ENDPOINT = process.env.NODE_ENV === "development" ? "http:/localhost:4001" : "https://collaboration-lab.herokuapp.com/";
+const ENDPOINT = process.env.NODE_ENV === "development" ? "http://localhost:4001" : "https://collaboration-lab.herokuapp.com/";
 // create random user
 //const user = "User_" + String(new Date().getTime()).substring(-3);
 
