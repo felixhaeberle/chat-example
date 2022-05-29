ebf626cc6241fc0bbf184f5fafe40cd94edb96d0
Felix Haeberle <kontakt@felixhaeberle.de>
2022-05-29 16:29:56 +0200

    update env logging

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { getSimilarColors, stringToColor } from "./helpers/colors";
 import io from "socket.io-client";
 
 let socket;
+console.log(process.env.NODE_ENV)
 const ENDPOINT = process.env.NODE_ENV === "development" ? "http://localhost:4001" : "https://collaboration-lab.herokuapp.com:4001";
 // create random user
 //const user = "User_" + String(new Date().getTime()).substring(-3);
