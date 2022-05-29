ee79088e68df6d4faf2daa5cfb3c1c952d8fc0c4
Felix Haeberle <kontakt@felixhaeberle.de>
2022-05-29 17:56:27 +0200

    add names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ const ENDPOINT = process.env.NODE_ENV === "development" ? "http://localhost:4001
 
 function App() {
   const [cursorPosition, setCursorPosition] = useState({x: 100, y: 100});
+  const [name, setName] = useState('');
   // eslint-disable-next-line
   const [cursors, setCursors] = useState([]);
 
@@ -25,6 +26,7 @@ function App() {
         console.log(cursors[index]);
         cursors[index].x = data.x;
         cursors[index].y = data.y;
+        cursors[index].name = data.name;
         cursors[index].color = getSimilarColors(stringToColor(String(cursors[index].socket)));
       } else if (data.socket) {
         cursors.push(data);
@@ -34,26 +36,33 @@ function App() {
   }, [cursors]);
 
   const handleMouseChange = (e) => {
-    setCursorPosition({x: e.pageX, y: e.pageY, socket: socket.id});
+    setCursorPosition({x: e.pageX, y: e.pageY, socket: socket.id, name: name ? name : ''});
     socket.emit("cursor_position", cursorPosition);
   }
 
   return (
     <div style={{ height: '100vh', width: '100vw'}} onMouseMove={(e) => handleMouseChange(e)}>
+      <input value={name} onChange={(e) => {
+        setName(e.target.value)
+      }} />
       { cursors.length !== 0 && cursors.map((c) => {
         if(c.socket === socket.id) return null
         return (
-          <svg
-          style={{ height:'50px', width: '50px', position: 'absolute', top: c ? c.y : null, left: c ? c.x : null }}
-          xmlns="http://www.w3.org/2000/svg"
-          x="0"
-          y="0"
-          version="1.1"
-          viewBox="0 0 28 28"
-          xmlSpace="preserve"
-        >
-          <path fill={'#' + c.color} d="M9.2 7.3L9.2 18.5 12.2 15.6 12.6 15.5 17.4 15.5z"></path>
-        </svg>  
+          <div style={{ position: 'absolute', top: c ? c.y : null, left: c ? c.x : null }}>
+            <svg
+              style={{ height:'50px', width: '50px' }}
+              xmlns="http://www.w3.org/2000/svg"
+              x="0"
+              y="0"
+              version="1.1"
+              viewBox="0 0 28 28"
+              xmlSpace="preserve"
+            >
+              <path fill={'#' + c.color} d="M9.2 7.3L9.2 18.5 12.2 15.6 12.6 15.5 17.4 15.5z"></path>
+              
+            </svg>
+            {c.name !== '' ? <span>{ c.name }</span> : null}  
+          </div>
         )
       }) }
     </div>
